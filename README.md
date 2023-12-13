37078328a2719dc28a94c55a63ab46850bc777d9
GiraKoo <girakoo@163.com>
2023-12-13 13:47:00 +0800

    upload replace_apt_source.py

--- README.md
+++ README.md
@@ -1,25 +1,19 @@
-# utoys
+# i-toys
 
 #### 介绍
-{**以下是 Gitee 平台说明，您可以替换此简介**
-Gitee 是 OSCHINA 推出的基于 Git 的代码托管平台（同时支持 SVN）。专为开发者提供稳定、高效、安全的云端软件开发协作平台
-无论是个人、团队、或是企业，都能够用 Gitee 实现代码托管、项目管理、协作开发。企业项目请看 [https://gitee.com/enterprises](https://gitee.com/enterprises)}
 
-#### 软件架构
-软件架构说明
-
-
-#### 安装教程
-
-1.  xxxx
-2.  xxxx
-3.  xxxx
+i-toys是个人使用的脚本集合。将一些常用的操作整合成脚本，一键执行。
 
 #### 使用说明
 
+- replace_apt_source.py
+  - 替换ubuntu的apt源，便于加速ubuntu的安装和升级。
+  - `/etc/apt/sources.list`将备份到`/etc/apt/sources.list.izmj.bak`。如果备份已经存在，则不重复备份。
+  - 需要sudo权限。
+  - 脚本原理是搜索http.*.ubuntu.com，替换为指定源https://mirrors.izmj.net。
+  - 自定义原地址时，请确保路径与预置的源格式一致。
 1.  xxxx
 2.  xxxx
-3.  xxxx
 
 #### 参与贡献
 
@@ -27,13 +21,3 @@ Gitee 是 OSCHINA 推出的基于 Git 的代码托管平台（同时支持 SVN�
 2.  新建 Feat_xxx 分支
 3.  提交代码
 4.  新建 Pull Request
-
-
-#### 特技
-
-1.  使用 Readme\_XXX.md 来支持不同的语言，例如 Readme\_en.md, Readme\_zh.md
-2.  Gitee 官方博客 [blog.gitee.com](https://blog.gitee.com)
-3.  你可以 [https://gitee.com/explore](https://gitee.com/explore) 这个地址来了解 Gitee 上的优秀开源项目
-4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
-5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
-6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
---
+++ linux/replace_apt_source.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# 如果当前运行环境是python2，则退出。
+import sys
+if sys.version_info.major != 3:
+    print('\033[91m[ERROR] This script is for python3 only.\033[0m')
+    exit(0)
+
+# 如果当前系统不是Linux，则退出。
+import platform
+if platform.system() != 'Linux':
+    print('\033[91m[ERROR] This script is for debian only.\033[0m')
+    exit(0)
+
+# 如果没有sudo权限，则申请sudo权限。
+import os
+if os.getuid() != 0:
+    os.system('sudo ' + ' '.join(sys.argv))
+    exit(0)
+
+print("======== 欢迎使用izmj/itoys替换apt源脚本 ========")
+
+# 如果没有source.list文件，则退出。
+if not os.path.exists('/etc/apt/sources.list'):
+    print('\033[91m[ERROR] 文件[/etc/apt/sources.list]未找到！\033[0m')
+    exit(0)
+
+# 如果没有备份，则备份。
+if not os.path.exists('/etc/apt/sources.list.izmj.bak'):
+    print('>>> 备份[/etc/apt/sources.list] --> [/etc/apt/sources.list.izmj.bak]')
+    os.system('cp /etc/apt/sources.list /etc/apt/sources.list.izmj.bak')
+else:
+    print('>>> 文件[/etc/apt/sources.list.izmj.bak]已经存在，跳过备份。')
+    # 如果多次替换，可能会搜索不到关键字，导致替换失败，所以每次替换前，先恢复备份。
+    os.system('cp /etc/apt/sources.list.izmj.bak /etc/apt/sources.list')
+
+# 选择源。
+source_table = [
+    ["阿里源", "https://mirrors.aliyun.com"],
+    ["清华源", "https://mirrors.tuna.tsinghua.edu.cn"],
+    ["中科大源", "https://mirrors.ustc.edu.cn"],
+    ["华为源", "https://mirrors.huaweicloud.com"],
+    ["浙大源", "https://mirrors.zju.edu.cn"],
+    ["搜狐源", "https://mirrors.sohu.com"],
+    ["网易源", "https://mirrors.163.com"]
+]
+
+print('\n======== Ubuntu的apt镜像源一览：======== ')
+for i in range(len(source_table)):
+    print('  ' + str(i + 1) + '. ' + source_table[i][0] + ' (' + source_table[i][1] + ')')
+print('  ' + str(len(source_table) + 1) + '. ' + '自定义')
+source_choice = input('请选择源：')
+
+source_url = ''
+if source_choice == str(len(source_table) + 1):
+    source_url = input('请输入源的URL：')
+    if source_url == '':
+        print('\033[91m[ERROR] 自定义源的URL不能为空！\033[0m')
+        exit(0)
+    else:
+        print('>>> 已选择源：' + source_url)
+else:
+    if source_choice == '':
+        print('\033[91m[ERROR] 选择的源不存在！\033[0m')
+        exit(0)
+    source_choice_int = int(source_choice)
+    if (source_choice_int > 0 and source_choice_int <= len(source_table)):
+        source_url = source_table[source_choice_int - 1][1]
+        print('>>> 已选择源：' + source_table[i][0] + ' (' + source_table[i][1] + ')')
+    else:
+        print('\033[91m[ERROR] 选择的源不存在！\033[0m')
+        exit(0)
+
+# 替换源
+confirm = input('是否确认替换源？(y/n)')
+if confirm == 'y':
+    print('>>> 替换中...')
+    os.system('sed -i "s#http.*.ubuntu.com#'+ source_url +'#g" /etc/apt/sources.list')
+    print('>>> 替换完成！')
+else:
+    print('>>> 已取消替换！')
+    exit(0)
+
+# 更新源
+confirm = input('是否更新软件信息？(y/n)')
+if confirm == 'y':
+    print('>>> 更新中...')
+    os.system('apt update')
+    print('>>> 更新完成！')
+else:
+    print('>>> 已取消更新！')
+    exit(0)
+
+# 更新软件
+confirm = input('是否升级软件？(y/n)')
+if confirm == 'y':
+    print('>>> 升级中...')
+    os.system('apt upgrade')
+    print('>>> 升级完成！')
+else:
+    print('>>> 已取消升级！')
+    exit(0)
+
+print('======== 感谢使用izmj/itoys替换apt源脚本 ========')
